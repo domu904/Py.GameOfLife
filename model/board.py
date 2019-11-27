8bb3b629c1f496d868448e1861acf6b988a98397
otaku904 <domu904@gmail.com>
2019-11-26 20:48:22 -0600

    update the board models

--- model/board.py
+++ model/board.py
@@ -1,4 +1,10 @@
 class Board:
+    boardGrids = []
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def initializeGrids(self):
+        for i in range(20):
+            boardGrids.append(i)
