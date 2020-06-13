47222a87fa4c107f38519300cda0dde9c36f08be
otaku904 <domu904@gmail.com>
2020-06-13 12:00:20 -0500

    updated cell unit test

--- test/cellTest.py
+++ test/cellTest.py
@@ -1,9 +1,21 @@
 import unittest
 
+from model import cell
 
 class CellTest(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_cell_has_x_position(self):
+        a = False;
+        myCell = Cell(3,3)
+        if myCell.x > 0:
+            a = True
+        self.assertEqual(True, a)
+
+    def test_cell_has_y_position(self):
+        a = False
+        myCell = Cell(3, 3)
+        if myCell.y > 0:
+            a = True
+        self.assertEqual(True, a)
 
 
 if __name__ == '__main__':
