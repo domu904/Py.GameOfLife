0659eef1d90d6b1f665a4061bcd118a9670f2c51
otaku904 <domu904@gmail.com>
2021-01-17 20:22:04 -0600

    Update coordinates.py

--- coordinates.py
+++ coordinates.py
@@ -1,6 +1,10 @@
-class coordnates:
+class coordinates:
     def currentCoordinate():
+        pass
     
     def nextCoordinate():
+        pass
     
     def previousCoordinate():
+        pass
+
